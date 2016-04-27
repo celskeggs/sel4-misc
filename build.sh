c3f4e4c77a89a9c7698f2f26ffdd9b34a9797ee8
Cel Skeggs <git@celskeggs.com>
2016-04-26 20:49:29 -0700

    Quick fixes for CMake build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 make kernel_elf
-if [ ! -e cbuild ]; then mkdir cbuild; cmake ..; fi
+if [ ! -e cbuild ]; then mkdir cbuild; cd cbuild; cmake ..; cd ..; fi
 cd cbuild
 make
 make install

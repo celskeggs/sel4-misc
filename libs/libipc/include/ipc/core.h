db9d0cb92cd9a980605fe8084d0a932c2f22b660
Cel Skeggs <git@celskeggs.com>
2016-10-08 01:48:51 -0400

    Expand IPC system to include cap transfer and handle errors better

--- libs/libipc/include/ipc/core.h
+++ libs/libipc/include/ipc/core.h
@@ -5,71 +5,112 @@
 #include <bedrock/bedrock.h>
 #include <bedrock/errx.h>
 
-bool perform_ipc(seL4_CPtr ep, uint32_t tag, void *in, size_t in_len, void *out, size_t out_len);
+#define IPC_ERROR_GERR_BASE 256
 
-#define DECLARE_IPC(ipcname, fields_in, fields_out) \
+bool perform_ipc(seL4_CPtr ep, seL4_CPtr cap_in, seL4_CPtr cap_out, uint32_t tag, void *in, size_t in_len, void *out,
+                 size_t out_len);
+
+#define _DECLARE_IPC(ipcname, cptrs, cptrsp, cptrrev, cptri, fields_in, fields_out) \
 struct ipc_in_##ipcname { \
     fields_in \
 }; \
 struct ipc_out_##ipcname { \
     fields_out \
 }; \
-static inline bool ipc_##ipcname(seL4_CPtr ep, struct ipc_in_##ipcname *in, struct ipc_out_##ipcname *out) {\
-    return perform_ipc(ep, IPC_##ipcname, in, sizeof(*in), out, sizeof(*out));\
+static inline bool ipc_##ipcname(seL4_CPtr ep cptrs, struct ipc_in_##ipcname *in, struct ipc_out_##ipcname *out) {\
+    return perform_ipc(ep, cptrsp, IPC_##ipcname, in, sizeof(*in), out, sizeof(*out));\
 } \
-bool ipc_handle_##ipcname(uint32_t sender, struct ipc_in_##ipcname *in, struct ipc_out_##ipcname *out); \
+static int ipc_cap_meta_##ipcname = cptri; \
+bool ipc_handle_##ipcname(uint32_t sender cptrs, struct ipc_in_##ipcname *in, struct ipc_out_##ipcname *out); \
+static inline bool ipc_handle_wrap_##ipcname(uint32_t sender, seL4_CPtr cap_in, seL4_CPtr cap_out, void *in, void *out) { \
+    (void) ipc_cap_meta_##ipcname; \
+    (void) cap_in; \
+    (void) cap_out; \
+    return ipc_handle_##ipcname(sender cptrrev, (struct ipc_in_##ipcname *) in, (struct ipc_out_##ipcname *) out); \
+} \
 seL4_CompileTimeAssert(sizeof(struct ipc_in_##ipcname) <= seL4_MsgMaxLength); \
 seL4_CompileTimeAssert(sizeof(struct ipc_out_##ipcname) <= seL4_MsgMaxLength); \
 seL4_CompileTimeAssert((sizeof(struct ipc_in_##ipcname) & 3) == 0); \
 seL4_CompileTimeAssert((sizeof(struct ipc_out_##ipcname) & 3) == 0);
 
+#define __COMMA ,
+#define DECLARE_IPC(ipcname, fields_in, fields_out) \
+    _DECLARE_IPC(ipcname, , seL4_CapNull __COMMA seL4_CapNull, , 0, fields_in, fields_out)
+#define DECLARE_IPC_CI(ipcname, fields_in, fields_out) \
+    _DECLARE_IPC(ipcname, __COMMA seL4_CPtr cap_in, cap_in __COMMA seL4_CapNull, __COMMA cap_in, 1, fields_in, fields_out)
+#define DECLARE_IPC_CO(ipcname, fields_in, fields_out) \
+    _DECLARE_IPC(ipcname, __COMMA seL4_CPtr cap_out, seL4_CapNull __COMMA cap_out, __COMMA cap_out, 2, fields_in, fields_out)
+#define DECLARE_IPC_CIO(ipcname, fields_in, fields_out) \
+    _DECLARE_IPC(ipcname, __COMMA seL4_CPtr cap_in, __COMMA seL4_CPtr cap_out, cap_in __COMMA cap_out, __COMMA cap_in __COMMA cap_out, 3, fields_in, fields_out)
+
 #define SERVER_FOR(ipcname) \
     case IPC_##ipcname: \
-        if (length != sizeof(struct ipc_in_##ipcname)) {\
+        if (length != sizeof(struct ipc_in_##ipcname)) { \
             response = -GERR_UNSATISFIED_CONSTRAINT; \
-        } else if (ipc_handle_##ipcname(sender, (struct ipc_in_##ipcname *) data_buffer_in, (struct ipc_out_##ipcname *) data_buffer_out)) { \
+        } else if (caps != (ipc_cap_meta_##ipcname & 1)) { /* cap count mismatch */\
+            response = -GERR_UNSATISFIED_CONSTRAINT; \
+        } else if (ipc_handle_wrap_##ipcname(sender, recv_cptr, send_cptr, data_buffer_in, data_buffer_out)) { \
             response = 0; \
             response_len = sizeof(struct ipc_out_##ipcname); \
+            if (ipc_cap_meta_##ipcname & 3) { \
+                response_caps = 1; \
+            } \
         } else { /* failed */ \
             ERRX_TRACEPOINT; \
+            DEBUG("responding to IPC with error:"); \
+            ERRX_TRACEBACK; \
             /* TODO: consider printing traceback here */ \
             if (errx.type == errx_type_sel4 && errx.extra > 0) { \
                 response = (uint32_t) errx.extra; \
             } else if (errx.type == errx_type_generic && errx.extra > 0) { \
-                response = (uint32_t) -errx.extra; \
+                response = IPC_ERROR_GERR_BASE + (uint32_t) errx.extra; \
             } else { \
-                response = -GERR_UNKNOWN_ERROR; \
+                response = IPC_ERROR_GERR_BASE + GERR_UNKNOWN_ERROR; \
             } \
             ERRX_CONSUME; \
         } \
         break;
 
 #define SERVER_LOOP(ep, condition, handlers) \
-    while (condition) { \
-        ERRX_START; \
-        uint32_t data_buffer_in[seL4_MsgMaxLength >> 2]; \
-        uint32_t data_buffer_out[seL4_MsgMaxLength >> 2]; \
-        uint32_t sender; \
-        seL4_MessageInfo_t recv = seL4_Recv(ep, &sender); \
-        uint32_t label = seL4_MessageInfo_get_label(recv); \
-        uint32_t length = seL4_MessageInfo_get_length(recv); \
-        uint32_t response, response_len = 0; \
-        if (length > seL4_MsgMaxLength) { length = seL4_MsgMaxLength; } \
-        for (uint32_t i = 0; i < (length >> 2); i++) { \
-            data_buffer_in[i] = seL4_GetMR(i); \
+    { \
+        seL4_CPtr recv_cptr = cslot_alloc(), send_cptr = cslot_alloc(); \
+        assert(recv_cptr != seL4_CapNull && send_cptr != seL4_CapNull); /* todo: handle this with proper error handling */ \
+        while (condition) { \
+            ERRX_START; \
+            uint32_t data_buffer_in[seL4_MsgMaxLength >> 2]; \
+            uint32_t data_buffer_out[seL4_MsgMaxLength >> 2]; \
+            uint32_t sender; \
+            assert(cslot_set_receive_path(recv_cptr)); \
+            seL4_MessageInfo_t recv = seL4_Recv(ep, &sender); \
+            uint32_t label = seL4_MessageInfo_get_label(recv); \
+            uint32_t length = seL4_MessageInfo_get_length(recv); \
+            uint32_t caps = seL4_MessageInfo_get_extraCaps(recv); \
+            uint32_t response, response_len = 0, response_caps = 0; \
+            if (length > seL4_MsgMaxLength) { length = seL4_MsgMaxLength; } \
+            for (uint32_t i = 0; i < (length >> 2); i++) { \
+                data_buffer_in[i] = seL4_GetMR(i); \
+            } \
+            switch (label) { \
+                handlers \
+            default: \
+                DEBUG("specified label was not found in server's domain"); \
+                response = IPC_ERROR_GERR_BASE + GERR_UNSUPPORTED_OPTION; \
+                break; \
+            } \
+            ERRX_START; \
+            for (uint32_t i = 0; i < (response_len >> 2); i++) { \
+                seL4_SetMR(i, data_buffer_out[i]); \
+            } \
+            if (response_caps) { \
+                seL4_SetCap(0, send_cptr); \
+                DEBUG("SEND"); \
+            } \
+            seL4_Reply(seL4_MessageInfo_new(response, 0, response_caps, response_len & ~3)); \
+            assert(cslot_delete(send_cptr)); \
+            assert(cslot_delete(recv_cptr)); \
         } \
-        switch (label) { \
-            handlers \
-        default: \
-            response = -GERR_UNSUPPORTED_OPTION; \
-            break; \
-        } \
-        ERRX_START; \
-        for (uint32_t i = 0; i < (response_len >> 2); i++) { \
-            seL4_SetMR(i, data_buffer_out[i]); \
-        } \
-        seL4_Reply(seL4_MessageInfo_new(response, 0, 0, response_len & ~3)); \
+        cslot_free(recv_cptr); \
+        cslot_free(send_cptr); \
     }
-
 
 #endif //LIB_IPC_CORE_H
--- libs/libipc/src/core.c
+++ libs/libipc/src/core.c
@@ -1,7 +1,9 @@
 #include <bedrock/bedrock.h>
-#include <ipc/ipc.h>
+#include <ipc/core.h>
+#include <resource/cslot.h>
 
-bool perform_ipc(seL4_CPtr ep, uint32_t tag, void *params, size_t param_len, void *response, size_t response_len) {
+bool perform_ipc(seL4_CPtr ep, seL4_CPtr cap_in, seL4_CPtr cap_out, uint32_t tag, void *params, size_t param_len,
+                 void *response, size_t response_len) {
     if (params == NULL || response == NULL || ep == seL4_CapNull) {
         ERRX_RAISE_GENERIC(GERR_NULL_VALUE);
         return false;
@@ -14,28 +16,60 @@ bool perform_ipc(seL4_CPtr ep, uint32_t tag, void *params, size_t param_len, voi
         ERRX_RAISE_GENERIC(GERR_REQUEST_TOO_LARGE);
         return false;
     }
+    seL4_CPtr cap_out_false;
+    if (cap_out == seL4_CapNull) {
+        cap_out_false = cslot_alloc();
+        if (cap_out_false == seL4_CapNull) {
+            ERRX_TRACEPOINT;
+            return false;
+        }
+        if (!cslot_set_receive_path(cap_out_false)) {
+            ERRX_TRACEPOINT;
+            return false;
+        }
+    } else {
+        cap_out_false = seL4_CapNull;
+        if (!cslot_set_receive_path(cap_out)) { // TODO: reset these to something that can't clobber afterward
+            ERRX_TRACEPOINT;
+            return false;
+        }
+    }
     uint32_t *param32 = (uint32_t *) params;
     for (uint32_t i = 0; i < (param_len >> 2); i++) {
         seL4_SetMR(i, param32[i]);
     }
-    seL4_MessageInfo_t resp = seL4_Call(ep, seL4_MessageInfo_new(tag, 0, 0, param_len));
-    int32_t err = seL4_MessageInfo_get_label(resp);
+    if (cap_in != seL4_CapNull) {
+        seL4_SetCap(0, cap_in);
+    }
+    seL4_MessageInfo_t resp = seL4_Call(ep, seL4_MessageInfo_new(tag, 0, cap_in == seL4_CapNull ? 0 : 1, param_len));
+    uint32_t err = seL4_MessageInfo_get_label(resp);
+    uint32_t caps = seL4_MessageInfo_get_extraCaps(resp);
     uint32_t length = seL4_MessageInfo_get_length(resp);
+    bool out = false;
     if (err == 0) { // success!
         uint32_t *resp32 = (uint32_t *) response;
         if (length != response_len) {
             ERRX_RAISE_GENERIC(GERR_DATA_SPILLED);
-            return false;
+        } else {
+            if ((cap_out != seL4_CapNull) != (caps != 0)) {
+                DEBUG("data spill occurred");
+                debug_printhex(cap_out);
+                debug_printhex(caps);
+                ERRX_RAISE_GENERIC(GERR_DATA_SPILLED);
+            } else {
+                for (uint32_t i = 0; i < (response_len >> 2); i++) {
+                    resp32[i] = seL4_GetMR(i);
+                }
+                out = true;
+            }
         }
-        for (uint32_t i = 0; i < (response_len >> 2); i++) {
-            resp32[i] = seL4_GetMR(i);
-        }
-        return true;
-    } else if (err > 0) { // seL4-specified error.
+    } else if (err > 0 && err < IPC_ERROR_GERR_BASE) { // seL4-specified error.
         ERRX_RAISE_SEL4(err);
-        return false;
     } else { // GERR error
-        ERRX_RAISE_GENERIC(-err);
-        return false;
+        ERRX_RAISE_GENERIC(err - IPC_ERROR_GERR_BASE);
     }
+    if (cap_out_false != seL4_CapNull) {
+        cslot_free(cap_out_false);
+    }
+    return out;
 }

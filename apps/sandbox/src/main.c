8b88a99b5c44ce93914971b662fbde7a2573210e
Cel Skeggs <git@celskeggs.com>
2016-10-13 23:33:57 -0400

    Remove extraneous IPC test code

--- apps/sandbox/src/main.c
+++ apps/sandbox/src/main.c
@@ -5,13 +5,6 @@
 
 bool main(void) {
     debug_println("hello, sandbox world!");
-    struct ipc_out_ping out;
-    if (!ipc_ping(ecap_IOEP, &(struct ipc_in_ping) {.value = 102}, &out)) {
-        ERRX_TRACEPOINT;
-        return false;
-    }
-    assert(out.value_neg == ~102);
-    DEBUG("received!");
     struct mem_page_cookie cookie = {};
     if (!mem_page_map((void *) 0xFFFF0000, &cookie)) {
         ERRX_TRACEPOINT;

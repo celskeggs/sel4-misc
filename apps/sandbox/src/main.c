ca17ff9704856b117d19a9e3739360697c8348bc
Cel Skeggs <git@celskeggs.com>
2016-10-14 22:04:24 -0400

    Split out sandbox code for launching processes

--- apps/sandbox/src/main.c
+++ apps/sandbox/src/main.c
@@ -3,9 +3,11 @@
 #include <ipc/ipc.h>
 #include <resource/mem_page.h>
 #include <elfloader/elfexec.h>
+#include <resource/mem_fx.h>
 
 extern char *image_registrar;
 extern char *image_registrar_end;
+seL4_CPtr sandbox_ep = seL4_CapNull;
 
 bool ipc_handle_ping(uint32_t sender, struct ipc_in_ping *in, struct ipc_out_ping *out) {
     (void) sender;
@@ -35,6 +37,40 @@ bool ipc_handle_sandbox_ready(uint32_t sender, seL4_CPtr cap_in, struct ipc_in_s
     return false;
 }
 
+struct elfexec *launch_process(void *image, size_t length, uint8_t priority, uint32_t badge_number) {
+    struct elfexec *context = mem_fx_alloc(sizeof(struct elfexec));
+    if (context == NULL) {
+        ERRX_TRACEPOINT;
+        return NULL;
+    }
+    seL4_CPtr minted = cslot_alloc();
+    if (minted == seL4_CapNull) {
+        ERRX_TRACEPOINT;
+        mem_fx_free(context, sizeof(struct elfexec));
+        return NULL;
+    }
+    if (!cslot_mint(sandbox_ep, minted, badge_number)) {
+        cslot_free(minted);
+        mem_fx_free(context, sizeof(struct elfexec));
+        ERRX_TRACEPOINT;
+        return NULL;
+    }
+    if (!elfexec_init(image_registrar, image_registrar_end - image_registrar, context, priority, minted)) {
+        cslot_free(minted);
+        mem_fx_free(context, sizeof(struct elfexec));
+        ERRX_TRACEPOINT;
+        return NULL;
+    }
+    cslot_free(minted);
+    if (!elfexec_start(context)) {
+        elfexec_destroy(context);
+        mem_fx_free(context, sizeof(struct elfexec));
+        ERRX_TRACEPOINT;
+        return NULL;
+    }
+    return context;
+}
+
 bool main(void) {
     debug_println("hello, sandbox world!");
     object_token ep = object_alloc(seL4_EndpointObject);
@@ -42,34 +78,15 @@ bool main(void) {
         ERRX_TRACEPOINT;
         return false;
     }
-    seL4_CPtr minted = cslot_alloc();
-    if (minted == seL4_CapNull) {
+    sandbox_ep = object_cap(ep);
+    // throwing away registrar pointer - not going to free it.
+    if (launch_process(image_registrar, image_registrar_end - image_registrar, 255, 1) == NULL) {
+        sandbox_ep = seL4_CapNull;
         object_free_token(ep);
         ERRX_TRACEPOINT;
         return false;
     }
-    if (!cslot_mint(object_cap(ep), minted, 1123)) {
-        cslot_free(minted);
-        object_free_token(ep);
-        ERRX_TRACEPOINT;
-        return false;
-    }
-    struct elfexec context;
-    if (!elfexec_init(image_registrar, image_registrar_end - image_registrar, &context, 255, minted)) {
-        cslot_free(minted);
-        object_free_token(ep);
-        ERRX_TRACEPOINT;
-        return false;
-    }
-    cslot_free(minted);
-    if (!elfexec_start(&context)) {
-        object_free_token(ep);
-        ERRX_TRACEPOINT;
-        return false;
-    }
-    DEBUG("start");
-    seL4_CPtr cap = object_cap(ep);
-    SERVER_LOOP(cap, true, SERVER_FOR(ping)
+    SERVER_LOOP(sandbox_ep, true, SERVER_FOR(ping)
             SERVER_FOR(alloc)
             SERVER_FOR(free)
             SERVER_FOR(sandbox_ready))
